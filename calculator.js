43cbe402f72a006a535786dbbe63d20fe4620dad
PC Tech <systech@convertbetter.local>
2022-12-20 08:49:48 +0800

    calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+class Calculator {
+    addition(firstNumber, secondNumber) {
+        return +firstNumber + +secondNumber;
+    }
+
+    subtraction(firstNumber, secondNumber) {
+        return +firstNumber - +secondNumber;
+    }
+
+    division(firstNumber, secondNumber) {
+        return +firstNumber / +secondNumber;
+    }
+
+    multiplication(firstNumber, secondNumber) {
+        return +firstNumber * +secondNumber;  
+    }
+}
